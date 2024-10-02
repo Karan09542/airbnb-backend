6ab5a1399b3ab85f2e610f60e521a37da8b303d5
Karan09542 <Karan09542@gamil.com>
2024-10-02 09:04:43 +0530

    Config app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config({path: "./.env"})
 const app = express()
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173/"],
+    origin: process.env.FRONTEND_URL || "http://localhost:5173/",
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     credentials: true,  // Allow credentials (cookies, auth headers, etc.)
 }))
