8cdf31a7970e714666d12a183790932432fd404d
Karan09542 <Karan09542@gamil.com>
2024-10-05 15:47:27 +0530

    Add patch

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173/",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH"], // Allowed methods
     credentials: true,  // Allow credentials (cookies, auth headers, etc.)
 }))
 
